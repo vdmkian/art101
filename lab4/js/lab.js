fcd54b4138623bce49925ec19c0d60bf5722d305
vdmkian <vdmkian@gmail.com>
2021-10-13 20:30:14 -0700

    css stuff and lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,24 @@
+// Pseudocode for connecet 4 game.
+// connect 4.
+// @link   URL
+// @file   This files defines the MyClass class.
+// Kian van der MEer
+// @since  x.x.x
+
+//Create a connect 4 game
+//decide who goes first with randomizer
+//first player is assigned to readable
+//second player is assigned to yellow
+//create a 7x6 grid
+//if player1 turn then:
+//input number for grid
+//height=number of times row has been selected
+//put a red piece in the row equal to the number at height+1
+//if player2 turn then:
+//input number for grid
+//height=number of times row has been selected
+//put a yellow piece in the row equal to the number at height+1
+//if 4 yellow pices touch then:
+//player 2 wins
+//if 4 red pieces touch then:
+//player 1 wins
