1d2b63ea86b9c023946c82d27a30265b72cb5524
vdmkian <vdmkian@gmail.com>
2021-10-18 22:34:42 -0700

    lab 5 i did

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ *Author: kian van der Meer <kvanderm@ucsc.edu>
+ *created: 10/18/21
+ *license: public domain
+ */
+
+// Define variables
+make = "toyota";
+model = "carrola";
+color = "grey";
+year = 2003
+
+// calculate
+age = 2021 - year;
+
+// output
+document.writeln("make: " + make + "<br>"):
+document.writeln("model: " + model + "<br>"):
+document.writeln("color: " + color + "<br>"):
+document.writeln("year: " + year + "<br>"):
+document.writeln("age: " + age + " years<br>"):
